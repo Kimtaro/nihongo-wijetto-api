307b65d0fd03873a495a2d34875f7041ee182209
Kim Ahlström <kahlstrom@figma.com>
2023-08-08 18:15:15 -0700

    Api response should be an object

--- app/controllers/api/v1/sentences_controller.rb
+++ app/controllers/api/v1/sentences_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::SentencesController < ApplicationController
   def index
     sentences = TANAKA_DB.lookup(params[:word]) || []
     sentences = sentences[0..10]
-    render json: sentences
+    render json: {sentences: sentences}
   end
 end
